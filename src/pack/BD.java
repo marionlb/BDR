d9e73cebb47798ec7fa1d70687bb79743fab0178
marionlb <marion.le-bras@esial.net>
2012-12-09 10:56:46 -0500

    versions actuelles du travail sur les couts

--- src/pack/BD.java
+++ src/pack/BD.java
@@ -1,4 +1,4 @@
-
+package pack;
 
 import java.util.ArrayList;
 import java.util.HashMap;
--- src/pack/QueryOptimizer.java
+++ src/pack/QueryOptimizer.java
@@ -1,3 +1,5 @@
+package pack;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -15,11 +17,14 @@ import ca.uqac.dim.turtledb.Relation;
 import ca.uqac.dim.turtledb.Schema;
 import ca.uqac.dim.turtledb.Table;
 import ca.uqac.dim.turtledb.TableParser;
+import ca.uqac.dim.turtledb.Tuple;
 import ca.uqac.dim.turtledb.UnaryRelation;
 import ca.uqac.dim.turtledb.VariableTable;
 
 public class QueryOptimizer {
 
+	static HashMap<String,Float> varInterCouts;
+	static HashMap<String,Integer> varInterTuples;
 	/**
 	 * Les sites composant la base de données répartie
 	 */
@@ -31,17 +36,18 @@ public class QueryOptimizer {
 	 * des sites (i.e. quel fragment est hébergé sur quel site)
 	 */
 	List<VariableTable> listeFragments;
+	List<Table> listeTables;
 
 	/**
 	 * Tableau des coûts de stockage
 	 */
 	// float[] coutsStockage;
-	HashMap<String, Float> coutsStockage;
+	static HashMap<String, Float> coutsStockage;
 	/**
 	 * Matrice des coûts de communication
 	 */
 	// float[][] coutsComm;
-	Matrice coutsComm;
+	static Matrice coutsComm ;
 
 	/**
 	 * Calcule le coût d'un plan de requêtes donné, à partir des coûts de
@@ -51,47 +57,99 @@ public class QueryOptimizer {
 	 *            Le plan de requête qu'on analyse
 	 * @return Le coût total du plan de requêtes.
 	 */
-	public float getCost(QueryPlan qp) {
+	public static float getCost(QueryPlan qp) {
+		if(coutsStockage==null || coutsComm==null)
+			for (Engine site : BD.sites.values()) {
+				QueryOptimizer.addDefaultCost(site.getName());
+			}
+
 		float res = 0;
-		for (Iterator<Entry<String, Set<Relation>>> iterator = qp.entrySet()
-				.iterator(); iterator.hasNext();) {
-			Entry<String, Set<Relation>> type = iterator.next();
-			String nomSite = type.getKey();
-			Relation r = type.getValue().iterator().next();
-			res += getCost(nomSite, r);
+		varInterCouts = new HashMap<String, Float>();
+		for (Entry<String,Set<Relation>> entry : qp.entrySet()) {
+			res+=cost(entry.getKey(), entry.getValue());
+			System.out.println(res);
 		}
 		return res;
 	}
 
-	private float getCost(String site, Relation r) {
-		if (r instanceof VariableTable) {
-			VariableTable frag = (VariableTable) r;
-			if (frag.getSite().equals(site)) {
-				return 0;
-			} else {
-				String site2 = frag.getSite();
-				float coutStock = coutsStockage.get(site);
-				float coutComm = coutsComm.get(site, site2);
-				int tuplesDeplaces = listeSites.get(site).getTuplesReceived();
-				return tuplesDeplaces * (coutComm + coutStock);
-			}
+	public static float cost(String site, Set<Relation> set) {
+		float res=0;
+		if(!BD.sites.containsKey(site))
+			return -1;
+
+		for (Iterator<Relation> iterator = set.iterator(); iterator.hasNext();) {
+			Relation r = iterator.next();
+
+			if(r instanceof VariableTable && !BD.isATable((VariableTable) r)) {
+				VariableTable vt = (VariableTable)r;
+
+				res += calcCost(site,vt.getRelation());
+
+
+				if(vt.getRelation() instanceof VariableTable) {
+					String siteTransfert =  vt.getSite();
+					if(BD.sites.containsKey(siteTransfert)) {
+						float coutTransfert = cost(siteTransfert, site);
+						coutTransfert*=((VariableTable)vt.getRelation()).nTuples;
+						res+=coutTransfert;
+					}
+				}
+
+
+				varInterCouts.put( ((VariableTable)r).getName(), res);
+			} else
+				res = calcCost(site, r);
 		}
-		int souscout = 0;
-		if(r instanceof BinaryRelation) {
-			souscout+=getCost(site,((BinaryRelation)r).getLeft());
-			souscout+=getCost(site,((BinaryRelation)r).getRight());
-		} else if (r instanceof NAryRelation) {
-			for (Relation rel : ((NAryRelation)r).getM_relations()) {
-				souscout+=getCost(site, rel);
-			}
-		} else if (r instanceof UnaryRelation) {
-			
-		} else if (r instanceof Table) {
-			System.err.println("On a une Table dans l'évaluation des couts");
-		}
-		return 0;
+		return res;
 	}
 
+	private static float calcCost(String site,  Relation r) {
+		float res =0;
+
+		//cas Résultat intermédiaire feuille : cout déjà calculé
+		if(r instanceof VariableTable && !BD.isATable((VariableTable) r)) {
+			VariableTable vt = (VariableTable)r;
+			if(!varInterCouts.containsKey(vt.getName())) {
+				return -3;
+			}
+			float cost= varInterCouts.get(vt.getName());
+			//			res=cost;
+		} 
+		//cas Table : transfert des tuples
+		else if(/*r instanceof Table || */r instanceof VariableTable && BD.isATable((VariableTable) r)) {
+			//cout = nb tuples déplacés * cout de deplacement
+			//			Table table = (r instanceof Table ? (Table) r : BD.tables.get(((VariableTable)r).getName()));
+			Table table = BD.getTable((VariableTable)r);
+			if(BD.isHostedOn(table)==null)
+				return -2;
+			String[] siteNames = BD.isHostedOn(table).split("\n");
+			float coutMin = Float.MAX_VALUE;
+			for (String siteName : siteNames) {
+				System.out.println(siteName);
+				if(siteName.equals(site)) {
+					coutMin=0;
+					break;
+				}
+				else if (coutsComm.get(site, siteName)<coutMin) {
+					coutMin = coutsComm.get(site, siteName);
+				}
+			}
+			res=(coutMin+coutsStockage.get(site))*(table).tupleCount();
+			((VariableTable)r).nTuples=table.tupleCount();
+			((VariableTable)r).cost=res;
+
+		} else if(r instanceof BinaryRelation) {
+			res=calcCost(site,((BinaryRelation)r).getLeft());
+			res+=calcCost(site,((BinaryRelation)r).getRight());
+		} else if (r instanceof NAryRelation) {
+			for (Relation rel : ((NAryRelation)r).getM_relations()) {
+				res+=calcCost(site, rel);
+			}
+		} else if (r instanceof UnaryRelation) {
+			res=calcCost(site, ((UnaryRelation) r).getRelation());
+		}
+		return res;
+	}
 	/**
 	 * Calcule le meilleur plan de requêtes distribué possible pour une requête,
 	 * en matière de coût
@@ -105,67 +163,10 @@ public class QueryOptimizer {
 	}
 
 
-	// public QueryOptimizer(List<String> listeSites,
-	// List<VariableTable> listeFragments, float[] coutsStockage,
-	// float[][] coutsComm) {
-	// super();
-	// this.listeSites = listeSites;
-	// this.listeFragments = listeFragments;
-	// this.coutsStockage = coutsStockage;
-	// this.coutsComm = coutsComm;
-	// }
-
-	// //////////////////////////////////////////////
-	// ////////GETTERS ++ SETTERS////////////////////
-	// //////////////////////////////////////////////
-
-	public List<VariableTable> getListeFragments() {
-		return listeFragments;
-	}
-
-	public void setListeFragments(List<VariableTable> listeFragments) {
-		this.listeFragments = listeFragments;
-	}
-
-	// public float[] getCoutsStockage() {
-	// return coutsStockage;
-	// }
-	//
-	// public void setCoutsStockage(float[] coutsStockage) {
-	// this.coutsStockage = coutsStockage;
-	// }
-	//
-	// public float[][] getCoutsComm() {
-	// return coutsComm;
-	// }
-	//
-	// public void setCoutsComm(float[][] coutsComm) {
-	// this.coutsComm = coutsComm;
-	// }
-
-	public HashMap<String, Float> getCoutsStockage() {
-		return coutsStockage;
-	}
-
-	public void setCoutsStockage(HashMap<String, Float> coutsStockage) {
-		this.coutsStockage = coutsStockage;
-	}
-
-	public Matrice getCoutsComm() {
-		return coutsComm;
-	}
-
-	public void setCoutsComm(Matrice coutsComm) {
-		this.coutsComm = coutsComm;
-	}
-
 	static QueryOptimizer randomInit(int nbSites, int nbTables) {
 		// init
 		List<String> lnomSites = new ArrayList<String>();
 		List<VariableTable> lfrag = new ArrayList<VariableTable>();
-		float[] cStock = new float[nbSites];
-		float[][] cComm = new float[nbSites][nbSites];
-
 		// init des sites
 		for (int i = 0; i < nbSites; i++) {
 			lnomSites.add("Site" + i);
@@ -209,34 +210,59 @@ public class QueryOptimizer {
 		return null;
 	}
 
-	public static void main(String[] args) {
-		randomInit(4, 5);
+	public static float cost(String siteDest, String s2) {
+		return coutsStockage.get(siteDest)+coutsComm.get(siteDest, s2);
 	}
 
-	private class Matrice extends HashMap<String, Float> {
-		public Matrice() {
-			super();
-		}
+	public static void addDefaultCost(String site1) {
+		if(coutsStockage==null)
+			coutsStockage = new HashMap<String, Float>();
+		if(coutsComm==null)
+			coutsComm=new Matrice();
 
-		public Matrice(String csv) {
-			String[] tab = csv.split(",");
-			for (String string : tab) {
-				String[] trio = string.split("[ -]");
-				this.add(trio[0], trio[1], Float.parseFloat(trio[2]));
-			}
-		}
-
-		public void add(String site1, String site2, float cout) {
-			this.put(site1 + "-" + site2, cout);
-		}
-
-		public float get(String site1, String site2) {
-			if (this.containsKey(site1 + "-" + site2)) {
-				return this.get(site1 + "-" + site2);
-			} else if (this.containsKey(site2 + "-" + site1)) {
-				return this.get(site2 + "-" + site1);
-			}
-			return -1;
+		//cout stockage
+		coutsStockage.put(site1,(float) 0.01);
+		//cout comm
+		for (Engine site : BD.sites.values()) {
+			if(!site.getName().equals(site1))
+				coutsComm.set(site1, site.getName(), 1);
 		}
 	}
+}
+
+class Matrice extends HashMap<String, Float> {
+
+	public Matrice() {
+		super();
+		//		for (Engine site : BD.sites.values()) {
+		//			QueryOptimizer.addDefaultCost(site.getName());
+		//		}
+	}
+
+	private void add(String site1, String site2, float cout) {
+		this.put(site1 + "-" + site2, cout);
+	}
+
+	public float get(String site1, String site2) {
+		if (this.containsKey(site1 + "-" + site2)) {
+			return this.get(site1 + "-" + site2);
+		} else if (this.containsKey(site2 + "-" + site1)) {
+			return this.get(site2 + "-" + site1);
+		}
+		return -1;
+	}
+	public void set(String site1, String site2, float cout) {
+		if(!this.containsKey(site1 + "-" + site2) &&
+				!this.containsKey(site2 + "-" + site1) ) {
+			this.add(site1, site2, cout);
+		} else {
+			if(this.containsKey(site1)) {
+				this.remove(site1);
+			} else {
+				this.remove(site2);
+			}
+			this.add(site1, site2, cout);
+		}
+	}
+
 }
