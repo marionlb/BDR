b1cb11573387aca57f4bc274a0fe2745e56f234a
marionlb <marion.le-bras@esial.net>
2012-12-09 18:04:49 -0500

    Nouveaux fichiers de tests sur Space

--- src/ca/uqac/dim/turtledb/util/FileReadWrite.java
+++ src/ca/uqac/dim/turtledb/util/FileReadWrite.java
@@ -1,7 +1,11 @@
 package ca.uqac.dim.turtledb.util;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
 import java.util.Scanner;
 
 /**
@@ -42,5 +46,24 @@ public class FileReadWrite
 	public static String getFileContents(String filename) throws IOException
 	{
 		return getFileContents(filename, DEFAULT_ENCODING);
+	}
+	
+	// Pris sur le net :
+	// http://stackoverflow.com/questions/326390/how-to-create-a-java-string-from-the-contents-of-a-file
+	public static String readFile(String path) throws IOException
+	{
+		FileInputStream stream = new FileInputStream(new File(path));
+		try
+		{
+			FileChannel fc = stream.getChannel();
+			MappedByteBuffer bb = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc
+					.size());
+			/* Instead of using default, pass in a decoder. */
+			return Charset.defaultCharset().decode(bb).toString().replace(
+					System.getProperty("line.separator"), "");
+		} finally
+		{
+			stream.close();
+		}
 	}
 }
